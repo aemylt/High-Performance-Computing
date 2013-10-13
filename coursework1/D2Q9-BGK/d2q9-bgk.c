05cc1c821a76902e1ac4b9528a85a78511f6a785
Dominic Moylett <dm1905@babyblue2.cm.cluster>
2013-10-13 23:17:05 +0100

    Fixed compiler warnings and ran results.

--- coursework1/D2Q9-BGK/d2q9-bgk.c
+++ coursework1/D2Q9-BGK/d2q9-bgk.c
@@ -94,8 +94,7 @@ int initialise(const char* paramfile, const char* obstaclefile,
 int timestep(const t_param params, t_speed* cells, t_speed* tmp_cells, int* obstacles);
 int accelerate_flow(const t_param params, t_speed* cells, int* obstacles);
 int propagate(const t_param params, t_speed* cells, t_speed* tmp_cells);
-int rebound(const t_param params, t_speed* cells, t_speed* tmp_cells, int* obstacles);
-int collision(const t_param params, t_speed* cells, t_speed* tmp_cells, int* obstacles);
+int rebound_or_collision(const t_param params, t_speed* cells, t_speed* tmp_cells, int* obstacles);
 int write_values(const t_param params, t_speed* cells, int* obstacles, double* av_vels);
 
 /* finalise, including freeing up allocated memory */
