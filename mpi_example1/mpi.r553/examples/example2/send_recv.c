0810753cefe4a46b774fb0f341b9f3724b45db1c
Dominic Moylett <dm1905@babyblue2.cm.cluster>
2013-11-04 16:37:02 +0000

    Converted send_recv to linear exchange.

--- mpi_example1/mpi.r553/examples/example2/send_recv.c
+++ mpi_example1/mpi.r553/examples/example2/send_recv.c
@@ -11,11 +11,12 @@ int main(int argc, char* argv[])
 {
   int myrank;
   int size;
-  int dest;              /* destination rank for message */
-  int source;            /* source rank of a message */
-  int tag = 0;           /* scope for adding extra information to a message */
+  int left;              /* destination rank for message */
+  int right;            /* source rank of a message */
+  int sendtag = 0;           /* scope for adding extra information to a message */
+  int recvtag = 0;
   MPI_Status status;     /* struct used by MPI_Recv */
-  char message[BUFSIZ];
+  char message_sent[BUFSIZ], message_received[BUFSIZ];
 
   /* MPI_Init returns once it has started up processes */
   MPI_Init( &argc, &argv );
@@ -24,34 +25,28 @@ int main(int argc, char* argv[])
   MPI_Comm_size( MPI_COMM_WORLD, &size );
   MPI_Comm_rank( MPI_COMM_WORLD, &myrank );
 
+  sprintf(message_sent, "Come-in Danny-Boy, this is process %d!", myrank);
+
+  right = (myrank + 1) % size;
+  left = myrank - 1;
+  if (left < 0) left += size;
+
   /* 
   ** SPMD - conditionals based upon rank
   ** will also become ubiquitous
   */
   if (myrank % 2 == 0) {  /* this is _NOT_ the master process */
-    /* create a message to send, in this case a character array */
-    sprintf(message, "Come-in Danny-Boy, this is process %d!", myrank);
-    dest = (myrank + 1) % size;
-    /* 
-    ** Send our first message!
-    ** use strlen()+1, so that we include the string terminator, '\0'
-    */
-    MPI_Send(message,strlen(message)+1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
-    source = myrank - 1;
-    if (source < 0) source = size + source;
-    MPI_Recv(message, BUFSIZ, MPI_CHAR, source, tag, MPI_COMM_WORLD, &status);
-    printf("process %d recieved message: %s\n", myrank, message);
+    MPI_Sendrecv(message_sent,strlen(message_sent)+1, MPI_CHAR, left, sendtag, message_received, BUFSIZ, MPI_CHAR, left, recvtag, MPI_COMM_WORLD, &status);
+    printf("process %d recieved message: %s\n", myrank, message_received);
+    MPI_Sendrecv(message_sent,strlen(message_sent)+1, MPI_CHAR, right, sendtag, message_received, BUFSIZ, MPI_CHAR, right, recvtag, MPI_COMM_WORLD, &status);
+    printf("process %d recieved message: %s\n", myrank, message_received);
 
   }
   else {
-    source = myrank - 1;
-    if (source < 0) source = size + source;
-    MPI_Recv(message, BUFSIZ, MPI_CHAR, source, tag, MPI_COMM_WORLD, &status);
-    printf("process %d recieved message: %s\n", myrank, message);
-    sprintf(message, "Come-in Danny-Boy, this is process %d!", myrank);
-    dest = (myrank + 1) % size;
-    MPI_Send(message,strlen(message)+1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
-
+    MPI_Sendrecv(message_sent,strlen(message_sent)+1, MPI_CHAR, right, sendtag, message_received, BUFSIZ, MPI_CHAR, right, recvtag, MPI_COMM_WORLD, &status);
+    printf("process %d recieved message: %s\n", myrank, message_received);
+    MPI_Sendrecv(message_sent,strlen(message_sent)+1, MPI_CHAR, left, sendtag, message_received, BUFSIZ, MPI_CHAR, left, recvtag, MPI_COMM_WORLD, &status);
+    printf("process %d recieved message: %s\n", myrank, message_received);
   }
 
   /* don't forget to tidy up when we're done */
